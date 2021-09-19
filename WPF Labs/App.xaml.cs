ac4fa2ac9adeac3d84627061e0e4e9d33049887d
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2021-09-19 14:49:55 +0300

    Initial commit. First lab added

---
+++ WPF Labs/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_Labs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WPF Labs/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace WPF_Labs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(TextBox1.Text, out int num))
+            {
+                MessageBox.Show(  num % 2 == 0 ? "Number is Even" : "Number is Odd");
+            }
+            else
+            {
+                MessageBox.Show("'A' character count in text: " + TextBox1.Text.ToLower().Count(c => c == 'a'));
+            }
+           
+        }
+    }
+}
